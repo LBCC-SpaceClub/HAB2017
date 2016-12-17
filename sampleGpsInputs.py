19e56e066ea61c3eb4bfe8202991bdca07f1dca5
Levi Willmeth <levi.willmeth@gmail.com>
2016-12-16 21:57:51 -0800

    Early testing rx gps location and serial coms.  Finds bearing from rx (via shield) to tx (via serial).

--- sampleGpsInputs.py
+++ sampleGpsInputs.py
@@ -5,14 +5,37 @@ from time import sleep
 
 parser = argparse.ArgumentParser(description='Send sample gps data at regular intervals.')
 parser.add_argument('-p', action='store', dest='port', default='/dev/ttyACM0')
-parser.add_argument('-b', action='store', dest='baudRate', type=int, default=115200)
+parser.add_argument('-b', action='store', dest='baudRate', type=int, default=9600)
 parser.add_argument('-s', action='store', dest='delay', type=int, default=2)
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    ser = serial.Serial(args.port, args.baudRate)
-    while(1):
-        print "Sending string.."
-        ser.write('$GPRMC,081836,A,3751.65,S,14507.36,E,000.0,360.0,130998,011.3,E*62\n')
-        print '  ', ser.readline()
-        sleep(args.delay)
+    ser = serial.Serial(args.port, args.baudRate, timeout=10)
+    ser.flush()
+    while True:
+        ser.write('$GPGGA,043955.000,4432.2213,N,12315.3913,W,1,08,0.96,71.8,M,-21.0,M,,*68\n')
+        line = ser.readline()[:-2]
+        line = line.split(',')
+        print line
+        # sleep(2)
+        # For some reason sleep hangs the program every ~30 seconds.  Idk why yet.
+
+'''
+eg3. $GPGGA,hhmmss.ss,llll.ll,a,yyyyy.yy,a,x,xx,x.x,x.x,M,x.x,M,x.x,xxxx*hh
+1    = UTC of Position
+2    = Latitude
+3    = N or S
+4    = Longitude
+5    = E or W
+6    = GPS quality indicator (0=invalid; 1=GPS fix; 2=Diff. GPS fix)
+7    = Number of satellites in use [not those in view]
+8    = Horizontal dilution of position
+9    = Antenna altitude above/below mean sea level (geoid)
+10   = Meters  (Antenna height unit)
+11   = Geoidal separation (Diff. between WGS-84 earth ellipsoid and
+       mean sea level.  -=geoid is below WGS-84 ellipsoid)
+12   = Meters  (Units of geoidal separation)
+13   = Age in seconds since last update from diff. reference station
+14   = Diff. reference station ID#
+15   = Checksum
+'''
--- shipStation/shipStation.ino
+++ shipStation/shipStation.ino
@@ -1,41 +1,68 @@
 #include "TinyGPS++.h"
-#include "SoftwareSerial.h"
+#include <SoftwareSerial.h>
 
 #define DEBUGGING true
 
+static const int RXPin = 8, TXPin = 7;
+static const uint32_t GPSBaud = 9600;
+
 TinyGPSPlus TxGps;
 TinyGPSPlus RxGps;
-SoftwareSerial softSerial(8, 7);
+SoftwareSerial ss(RXPin, TXPin);
 
 void setup() {
   // initialize serial connection and wait for it to be ready
-  Serial.begin(115200);
+  Serial.begin(9600);
+  Serial.flush();
   while(!Serial) ;
-  Serial.println("Waiting for input...");
+
+  // initialize software serial connection
+  ss.begin(GPSBaud);
+  
+  Serial.println("Arduino booted up and ready for input..");
+}
+
+void displayInfo(){
+  // txLat, txLong, txAlt, rxlat, rxLong, rxAlt, bearing from rx to tx.
+  Serial.print(TxGps.location.lat(), 6); Serial.print(",");
+  Serial.print(TxGps.location.lng(), 6); Serial.print(",");
+  Serial.print(TxGps.altitude.meters()); Serial.print(",");
+  Serial.print(RxGps.location.lat(), 6); Serial.print(",");
+  Serial.print(RxGps.location.lng(), 6); Serial.print(",");
+  Serial.print(RxGps.altitude.meters()); Serial.print(",");
+  Serial.println(findBearing(RxGps, TxGps));
+}
+
+double findBearing(TinyGPSPlus A, TinyGPSPlus B){
+  // Returns bearing from point A to point B
+  // See http://www.igismap.com/formula-to-find-bearing-or-heading-angle-between-two-points-latitude-longitude/
+  double cosA = cos(A.location.lat());
+  double cosB = cos(B.location.lat());
+  double sinA = sin(A.location.lat());
+  double sinB = sin(B.location.lat());
+  double lamLong = A.location.lng()-B.location.lng();
+  return atan2(
+    cosB*sin(lamLong),
+    cosA*sinB-sinA*cosB*cos(lamLong)
+  );
+    
 }
 
 void loop() {
   // Wait for new data from the tracking laptop, and process it.
-  if(Serial.available() > 0){
+  while(Serial.available() > 0){
     TxGps.encode(Serial.read());
-  }
-
-  // Debugging: When the tracker is updated, display the new information
-  if(DEBUGGING && TxGps.location.isUpdated()){
-    Serial.print("Tx LAT=");  Serial.print(TxGps.location.lat(), 6);
-    Serial.print(", LONG=");  Serial.print(TxGps.location.lng(), 6);
-    Serial.print(", ALT=");   Serial.print(TxGps.altitude.meters());
-    
-    Serial.print("\tRx LAT=");  Serial.print(RxGps.location.lat(), 6);
-    Serial.print(", LONG=");  Serial.print(RxGps.location.lng(), 6);
-    Serial.print(", ALT=");   Serial.println(RxGps.altitude.meters());
   }
   
   // Update RXLoc using GPS shield on arduino
-  if(softSerial.available() > 0){
-    RxGps.encode(softSerial.read());
+  while(ss.available() > 0){
+    RxGps.encode(ss.read());
   }
-  
+    
   // Update IMU position and adjust steppers
   
+  // Debugging: When the tracker is updated, display the new information
+  if(DEBUGGING && (TxGps.location.isUpdated() || RxGps.location.isUpdated()) ){
+    displayInfo();
+  }
 }
