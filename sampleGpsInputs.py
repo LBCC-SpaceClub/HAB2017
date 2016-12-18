29290a3aaa8453a2c9d45c99382a86f6099260cd
Levi Willmeth <levi.willmeth@gmail.com>
2016-12-18 11:15:06 -0800

    Add haversine function to arduino to find distance in km to payload

--- sampleGpsInputs.py
+++ sampleGpsInputs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import argparse
 import serial
+import re
 from time import sleep
 
 parser = argparse.ArgumentParser(description='Send sample gps data at regular intervals.')
@@ -9,11 +10,40 @@ parser.add_argument('-b', action='store', dest='baudRate', type=int, default=960
 parser.add_argument('-s', action='store', dest='delay', type=int, default=2)
 args = parser.parse_args()
 
+def checkSum(sentence):
+    # Credit: http://doschman.blogspot.com/2013/01/calculating-nmea-sentence-checksums.html
+    # cksum = sentence[len(sentence) - 2:]
+    chksumdata = re.sub("(\n|\r\n)","", sentence[sentence.find("$")+1:sentence.find("*")])
+    csum = 0
+    for c in chksumdata:
+        csum ^= ord(c)
+    return hex(csum)[2:]
+
+def gmapsToNMEA(coord):
+    # Assumes a gps coordinate in the US.
+    coord = abs(coord)
+    DDD = int(coord)
+    MM = coord%1*60
+    cardinal = 'N' if abs(coord) < 90 else 'W'
+    return '{0}{1:.4f},{2}'.format(DDD, MM, cardinal)
+
+# use google earth DD.DDDDDD coordinates here:
+targetLat = 44.584902
+targetLng = -123.258529
+# Alt is in meters
+targetAlt = 1000
+
+nmea = '$GPGGA,043955.000,{0},{1},1,08,0.96,{2},M,-21.0,M,,*'.format(
+    gmapsToNMEA(targetLat), gmapsToNMEA(targetLng), targetAlt
+)
+nmea += checkSum(nmea)+'\n'
+print nmea
+
 if __name__ == "__main__":
     ser = serial.Serial(args.port, args.baudRate, timeout=10)
     ser.flush()
     while True:
-        ser.write('$GPGGA,043955.000,4432.2213,N,12315.3913,W,1,08,0.96,71.8,M,-21.0,M,,*68\n')
+        ser.write(nmea)
         line = ser.readline()[:-2]
         line = line.split(',')
         print line
--- shipStation/shipStation.ino
+++ shipStation/shipStation.ino
@@ -6,8 +6,8 @@
 static const int RXPin = 8, TXPin = 7;
 static const uint32_t GPSBaud = 9600;
 
-TinyGPSPlus TxGps;
-TinyGPSPlus RxGps;
+TinyGPSPlus PayloadGps;
+TinyGPSPlus StationGps;
 SoftwareSerial ss(RXPin, TXPin);
 
 void setup() {
@@ -22,37 +22,61 @@ void setup() {
   Serial.println("Arduino booted up and ready for input..");
 }
 
+double HaverSine(double lat1, double lon1, double lat2, double lon2){
+  // Returns distance in km between two gps coordinates
+  // Credit to https://github.com/RobTillaart/Arduino
+  const double ToRad = PI / 180.0;
+  const double R = 6371;   // radius earth in Km
+
+  double dLat = (lat2-lat1) * ToRad;
+  double dLon = (lon2-lon1) * ToRad;
+
+  double a = sin(dLat/2) * sin(dLat/2) +
+    cos(lat1 * ToRad) * cos(lat2 * ToRad) *
+    sin(dLon/2) * sin(dLon/2);
+
+  double c = 2 * atan2(sqrt(a), sqrt(1-a));
+
+  double d = R * c;
+  return d; // returns km!
+}
+
 void displayInfo(){
-  // txLat, txLong, txAlt, rxlat, rxLong, rxAlt, bearing from rx to tx.
-  Serial.print(TxGps.location.lat(), 6); Serial.print(",");
-  Serial.print(TxGps.location.lng(), 6); Serial.print(",");
-  Serial.print(TxGps.altitude.meters()); Serial.print(",");
-  Serial.print(RxGps.location.lat(), 6); Serial.print(",");
-  Serial.print(RxGps.location.lng(), 6); Serial.print(",");
-  Serial.print(RxGps.altitude.meters()); Serial.print(",");
+  Serial.print(PayloadGps.location.lat(), 6); Serial.print(",");
+  Serial.print(PayloadGps.location.lng(), 6); Serial.print(",");
+  Serial.print(PayloadGps.altitude.meters()); Serial.print(",");
+  Serial.print(StationGps.location.lat(), 6); Serial.print(",");
+  Serial.print(StationGps.location.lng(), 6); Serial.print(",");
+  Serial.print(StationGps.altitude.meters()); Serial.print(",");
   double courseTo = TinyGPSPlus::courseTo(
-    TxGps.location.lat(), TxGps.location.lng(),
-    RxGps.location.lat(), RxGps.location.lng()
+    StationGps.location.lat(), StationGps.location.lng(),
+    PayloadGps.location.lat(), PayloadGps.location.lng()
   );
   Serial.print(courseTo); Serial.print(",");
-  Serial.println(TinyGPSPlus::cardinal(courseTo));
+  Serial.print(TinyGPSPlus::cardinal(courseTo)); Serial.print(",");
+  
+  Serial.print(courseTo); Serial.print(",");
+  Serial.println(HaverSine(
+    StationGps.location.lat(), StationGps.location.lng(),
+    PayloadGps.location.lat(), PayloadGps.location.lng())
+  );
 }
 
 void loop() {
   // Wait for new data from the tracking laptop, and process it.
   while(Serial.available() > 0){
-    TxGps.encode(Serial.read());
+    PayloadGps.encode(Serial.read());
   }
   
   // Update RXLoc using GPS shield on arduino
   while(ss.available() > 0){
-    RxGps.encode(ss.read());
+    StationGps.encode(ss.read());
   }
     
   // Update IMU position and adjust steppers
   
   // Debugging: When the tracker is updated, display the new information
-  if(DEBUGGING && (TxGps.location.isUpdated() || RxGps.location.isUpdated()) ){
+  if(DEBUGGING && (PayloadGps.location.isUpdated() || StationGps.location.isUpdated()) ){
     displayInfo();
   }
 }
