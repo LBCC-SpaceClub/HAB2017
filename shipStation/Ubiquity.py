cae4a988c2bd3f37d4dd9a85cf08c6fea7d0404d
Levi Willmeth <levi.willmeth@gmail.com>
2017-04-08 16:27:45 -0700

    Begin writing Arduino class

--- shipStation/Ubiquity.py
+++ shipStation/Ubiquity.py
@@ -2,28 +2,28 @@
 import requests
 import json
 
-class Ubiquity:
+class Ubiquity(object):
     ''' Methods to get information from the Ubiuity radio modem '''
     _host = 'http://192.168.1.20'
     _user = 'ubnt'
     _pass = 'ubnt'
-    _loginPage = 'login.cgi'
-    _statusPage = 'status.cgi'
+    _loginPage = '/login.cgi'
+    _statusPage = '/status.cgi'
     _session = None
 
     def __init__(self):
         ''' Prep the object by starting a new session and logging in '''
         self._session = requests.Session()
-        session.get(self._host+self.loginPage)  # This line has magic sauce
+        self._session.get(self._host+self.loginPage)  # This line has magic sauce
         data={'uri': '','username': self._user,'password': self._pass}
         # Use files trick to post using multipart/form-data encoding.
-        session.post(
+        self._session.post(
             self._host+self.loginPage,
             files={k: (None, v) for k, v in data.items()},
             verify=False
         )
 
-    def getRSSI(session):
+    def getRSSI(self):
         ''' Returns the RSSI value (0-100) '''
-        pjson = json.loads(session.get(HOST+'/status.cgi'))
+        pjson = json.loads(self._session.get(HOST+self._statusPage))
         return pjson.['RSSI']
