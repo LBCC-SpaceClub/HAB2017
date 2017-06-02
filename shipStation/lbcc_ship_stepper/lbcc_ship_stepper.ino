060a7ed453678a21a965482bdd559e28aa8ce8bd
Levi Willmeth <levi.willmeth@gmail.com>
2017-06-01 21:38:43 -0700

    Change to tinygps+ library, accept payload location via serial

--- shipStation/lbcc_ship_stepper/lbcc_ship_stepper.ino
+++ shipStation/lbcc_ship_stepper/lbcc_ship_stepper.ino
@@ -1,7 +1,6 @@
 #include <SoftwareSerial.h>
 #include <Wire.h>
 #include <SPI.h>
-//#include <Adafruit_GPS.h>
 #include <TinyGPS++.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
@@ -9,21 +8,70 @@
 #include <math.h>
 
 #define DEBUGGING true
-#define INPUT_BUF_SIZE 128
+
+static const int RXPin = 8, TXPin = 7;
+static const uint32_t GPSBaud = 9600;
+uint32_t gpsTimer = millis();
+uint32_t imuTimer = gpsTimer;
+sensors_event_t event;           //Create a new local event instance
+uint8_t sys, gyro, accel, mag;   //Create local variables gyro, accel, mag
 
 // Initializes an instance of the BNO055 called bno with an I2C address of 55
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
 // Ultimate GPS shield uses RX and TX on pins 8 and 7
-SoftwareSerial mySerial(8, 7);
-Adafruit_GPS GPS(&mySerial);
-Adafruit_GPS payloadGPS = new Adafruit_GPS();
+SoftwareSerial ss(RXPin, TXPin);
 
-uint32_t gpsTimer = millis();
-uint32_t imuTimer = gpsTimer;
-sensors_event_t event;           //Create a new local event instance
-uint8_t sys, gyro, accel, mag;   //Create local variables gyro, accel, mag
-char inputBuffer[INPUT_BUF_SIZE];
+TinyGPSPlus trackerGPS;
+TinyGPSPlus payloadGPS;
+
+void setup()
+{
+  Serial.begin(115200); // laptop
+  ss.begin(GPSBaud);    // local GPS
+  while(!Serial){ ; }
+  Serial.println("Linn-Benton Community College, Eclipse 2017 payload tracker starting up..");
+  
+  // Set up BNO055 IMU
+  if(!bno.begin()){
+    Serial.println("Ooops, could not find BNO055... Check your wiring or I2C ADDR!");
+//    while(1);
+  } else {
+    Serial.println("BNO055 IMU detected..");
+  }
+//  bno.setExtCrystalUse(true);                     //Use the external clock in the IMU
+//  bno.setMode(bno.OPERATION_MODE_NDOF);
+}
+
+void loop()
+{
+  // If new info is available from the laptop, parse it!
+  while(Serial.available() > 0){
+    payloadGPS.encode(Serial.read());
+  }
+
+  // If new info is available from the Adafruit GPS Shield, parse it!
+  if (ss.available() > 0){
+    trackerGPS.encode(ss.read());
+  }
+
+  // Display GPS info about every 2 seconds
+  if (millis() - gpsTimer > 2000){
+    gpsTimer = millis(); // reset the timer
+    
+    print_location("local", &trackerGPS);
+    print_time("local", &trackerGPS);
+    
+    print_location("payload", &payloadGPS);
+    print_time("payload", &payloadGPS);
+  }
+  
+  // Display IMU info about 10 times per second
+  if (millis() - imuTimer > 100){
+    imuTimer = millis(); // reset the timer
+    // print_local_imu();
+  }
+}
 
 
 float findBearing(float tLat, float tLon, float pLat, float pLon)
@@ -46,20 +94,6 @@ float findBearing(float tLat, float tLon, float pLat, float pLon)
   return bearing;
 }
 
-int parseSerial()
-{
-  char c;
-  uint8_t i = 0;
-  // Wait for a leading tilde
-  while(Serial.available()){
-    if(Serial.read() == '~') break;
-  }
-  // Process the rest of the line
-  while(Serial.available() && i<INPUT_BUF_SIZE-1){
-    c = Serial.read();
-    
-  }
-}
 
 float degToRad(float deg)
 {
@@ -67,114 +101,88 @@ float degToRad(float deg)
   return deg * M_PI / 180;
 }
 
+
 float radToDeg(float rad)
 {
   // Takes radians, returns degrees
   return rad * 180.0 / M_PI;
 }
 
-void setup()
-{
-  Serial.begin(115200);
-  while(!Serial){ ; }
-  Serial.println("Linn-Benton Community College, Eclipse 2017 payload tracker starting up..");
-  
-  // Set up BNO055 IMU
-  if(!bno.begin()){
-    Serial.println("Ooops, could not find BNO055... Check your wiring or I2C ADDR!");
-//    while(1);
-  } else {
-    Serial.println("BNO055 IMU detected..");
-  }
-//  bno.setExtCrystalUse(true);                     //Use the external clock in the IMU
-//  bno.setMode(bno.OPERATION_MODE_NDOF);
 
-  // Set up Adafruit Ultimate GPS Shield
-  GPS.begin(9600);
-  // Set GPS to update position once per second
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
-  GPS.sendCommand(PMTK_API_SET_FIX_CTL_1HZ);
-  // Specify GPS output style
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  //GPS.sendCommand(PGCMD_ANTENNA);
-  delay(100);
+void print_imu(){
+  //Read the current calibration values from the IMU
+  bno.getCalibration(&sys, &gyro, &accel, &mag);
+  //Read the current positional values
+  bno.getEvent(&event);
+  
+  // Using quaternions
+  imu::Quaternion q = bno.getQuat();
+  q.normalize();
+  float temp = q.x();  q.x() = -q.y();  q.y() = temp;
+  q.z() = -q.z();
+  // Converted back to eulers
+  imu::Vector<3> euler = q.toEuler();
+  Serial.print("[IMU]");
+  Serial.print(-180/M_PI * euler.x());  // heading, nose-right is positive, z-axis points up
+  Serial.print(',');
+  Serial.print(-180/M_PI * euler.y());  // roll, rightwing-up is positive, y-axis points forward
+  Serial.print(',');
+  Serial.print(-180/M_PI * euler.z());  // pitch, nose-down is positive, x-axis points right
+  /*
+  Serial.print(event.orientation.x,2);
+  Serial.print(",");
+  Serial.print(event.orientation.y,2);
+  Serial.print(",");
+  Serial.print(event.orientation.z,2);
+  */
+  Serial.print(',');
+  Serial.print(sys);
+  Serial.print(',');
+  Serial.print(gyro);
+  Serial.print(',');
+  Serial.print(accel);
+  Serial.print(',');
+  Serial.println(mag);
 }
 
-void loop()
-{
-  //If new GPS data is ready, parse it!
-  if (GPS.newNMEAreceived()){
-    if (!GPS.parse(GPS.lastNMEA()))
-      return;
-  }
 
-  payloadGPS.parse("$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47");
-  Serial.print("Payload GPS: ");
-  Serial.print(payloadGPS.latitudeDegrees,7);
-  Serial.print(',');
-  Serial.print(payloadGPS.longitudeDegrees,7);
-  Serial.print(',');
-  Serial.println((int)(payloadGPS.altitude));
-
-  // Display GPS info about every 2 seconds
-  if (millis() - gpsTimer > 2000){
-    gpsTimer = millis(); // reset the timer
-    Serial.print("Time: ");
-    Serial.print(GPS.hour, DEC); Serial.print(':');
-    Serial.print(GPS.minute, DEC); Serial.print(':');
-    Serial.print(GPS.seconds, DEC); Serial.print(" on ");
-    Serial.print(GPS.day, DEC); Serial.print('/');
-    Serial.print(GPS.month, DEC);Serial.print("/20");
-    Serial.println(GPS.year, DEC);
-    if(GPS.fix){
-      // GPS data on one line
-      Serial.print("[GPS]");
-      Serial.print(GPS.latitudeDegrees,7);
-      Serial.print(',');
-      Serial.print(GPS.longitudeDegrees,7);
-      Serial.print(',');
-      Serial.println((int)(GPS.altitude));
-      // Serial.println((int)(GPS.altitude * 3.28084)); // converted m to feet
-    }else{
-      Serial.print("No GPS fix!  GPS Quality: ");
-      Serial.println(GPS.fixquality);
-    }
+void print_time(char* desc, TinyGPSPlus* gps){
+  // Prints local GPS time to serial
+  Serial.print(desc);
+  Serial.print(F(": "));
+  if (gps->date.isValid() && gps->time.isValid()){
+    Serial.print(gps->date.month());
+    Serial.print(F("/"));
+    Serial.print(gps->date.day());
+    Serial.print(F("/"));
+    Serial.print(gps->date.year());
+    if (gps->time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps->time.hour());
+    Serial.print(F(":"));
+    if (gps->time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps->time.minute());
+    Serial.print(F(":"));
+    if (gps->time.second() < 10) Serial.print(F("0"));
+    Serial.println(gps->time.second());
+  } else {
+    Serial.println(F("INVALID"));
   }
-//  // Display IMU info about 10 times per second
-//  if (millis() - imuTimer > 100){
-//    imuTimer = millis(); // reset the timer
-//    //Read the current calibration values from the IMU
-//    bno.getCalibration(&sys, &gyro, &accel, &mag);
-//    //Read the current positional values
-//    bno.getEvent(&event);
-//
-//    // Using quaternions
-//    imu::Quaternion q = bno.getQuat();
-//    q.normalize();
-//    float temp = q.x();  q.x() = -q.y();  q.y() = temp;
-//    q.z() = -q.z();
-//    // Converted back to eulers
-//    imu::Vector<3> euler = q.toEuler();
-//    Serial.print("[IMU]");
-//    Serial.print(-180/M_PI * euler.x());  // heading, nose-right is positive, z-axis points up
-//    Serial.print(',');
-//    Serial.print(-180/M_PI * euler.y());  // roll, rightwing-up is positive, y-axis points forward
-//    Serial.print(',');
-//    Serial.print(-180/M_PI * euler.z());  // pitch, nose-down is positive, x-axis points right
-//    /*
-//    Serial.print(event.orientation.x,2);
-//    Serial.print(",");
-//    Serial.print(event.orientation.y,2);
-//    Serial.print(",");
-//    Serial.print(event.orientation.z,2);
-//    */
-//    Serial.print(',');
-//    Serial.print(sys);
-//    Serial.print(',');
-//    Serial.print(gyro);
-//    Serial.print(',');
-//    Serial.print(accel);
-//    Serial.print(',');
-//    Serial.println(mag);
-//  }
+}
+
+
+void print_location(char* desc, TinyGPSPlus* gps){
+  // Prints local GPS location to serial
+  Serial.print(desc);
+  Serial.print(F(": "));
+  if (gps->location.isValid()){
+    Serial.print(gps->location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps->location.lng(), 6);
+    Serial.print(F(","));
+    Serial.println(gps->altitude.meters());
+  }
+  else
+  {
+    Serial.println(F("INVALID"));
+  }
 }
